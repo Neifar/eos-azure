6b3381af8b6eb60cb125dc7304e334579cdc66d1
Ramzi NAIFAR <ramzineifar30@gmail.com>
2025-01-05 02:53:16 +0100

    add user service

---
+++ eos-user-api/src/main/java/com/org/proto/user/UserApplication.java
@@ -0,0 +1,14 @@
+package com.org.proto.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
